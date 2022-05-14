61fde1eb9d1fe40acee23dde6c9a1fdafe191797
thomerson <756155037@qq.com>
2022-05-14 13:44:22 +0800

    update

---
+++ Gatlin.Utils.Net/NetCore.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Net;
+
+namespace Gatlin.Utils.Net
+{
+    /// <summary>
+    /// 网络相关 
+    /// </summary>
+    public class NetCore
+    {
+        public static string GetIP()
+        {
+            var ipArr = Dns.GetHostAddresses(Dns.GetHostName());
+            return ipArr?.FirstOrDefault()?.ToString();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,9 +4,20 @@
 
 ## Email
 
+## NetCore
+* GetIP
 
 ## MD5
 
 * ```string```MD5加密
 * 获取文件MD5值
+
+## Logger
+
+自己写的日志记录，测试时或者很简单的程序中使用
+
+
+
+
+
 
